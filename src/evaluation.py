50496adf3d816b8794e6c61489affc82eb20df17
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2025-06-09 12:55:56 +0200

    minor fix evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -9,12 +9,3 @@ def evaluate_model(model, X_test, y_test,output_dir=None):
     print("Accuracy:", acc)
     if output_dir:
         save_confusion_matrix(y_test, y_pred,  save_path=f"{output_dir}/confusion_matrix.png")
-'''
-    # Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-    plt.show()
-'''
